71a4053d878f891479a279cd8e1c9332ff82263b
David Shaw <david.shaw1242@gmail.com>
2024-07-27 08:40:33 -0400

    formatting on output struct

--- models.go
+++ models.go
@@ -50,10 +50,10 @@ func databaseFeedToFeed(feed database.Feed) Feed {
 
 type FeedFollow struct {
 	ID			uuid.UUID 	`json:"id"`
-	CreatedAt	time.Time	`json:"created_at"`
-	UpdatedAt	time.Time	`json:"updated_at"`
 	FeedID		uuid.UUID	`json:"feed_id"`
 	UserID		uuid.UUID	`json:"user_id"`
+	CreatedAt	time.Time	`json:"created_at"`
+	UpdatedAt	time.Time	`json:"updated_at"`
 }
 
 func databaseFeedFollowToFeedFollow(feedFollow database.FeedUser) FeedFollow {
