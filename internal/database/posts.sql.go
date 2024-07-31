99dbe9b94a67412981369a7c60851e22b1e1503e
David Shaw <david.shaw1242@gmail.com>
2024-07-30 21:04:23 -0400

    created sql query to retrieve posts by user and then generated go code

--- internal/database/posts.sql.go
+++ internal/database/posts.sql.go
@@ -54,3 +54,53 @@ func (q *Queries) AddPost(ctx context.Context, arg AddPostParams) (Post, error) 
 	)
 	return i, err
 }
+
+const getPostsByUser = `-- name: GetPostsByUser :many
+SELECT id, created_at, updated_at, title, url, description, published_at, feed_id
+FROM posts
+WHERE feed_id IN (
+    SELECT feed_id
+    FROM feed_users
+    WHERE user_id = $1
+)
+ORDER BY published_at DESC
+LIMIT $2 OFFSET $3
+`
+
+type GetPostsByUserParams struct {
+	UserID uuid.UUID
+	Limit  int32
+	Offset int32
+}
+
+func (q *Queries) GetPostsByUser(ctx context.Context, arg GetPostsByUserParams) ([]Post, error) {
+	rows, err := q.db.QueryContext(ctx, getPostsByUser, arg.UserID, arg.Limit, arg.Offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Post
+	for rows.Next() {
+		var i Post
+		if err := rows.Scan(
+			&i.ID,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+			&i.Title,
+			&i.Url,
+			&i.Description,
+			&i.PublishedAt,
+			&i.FeedID,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
